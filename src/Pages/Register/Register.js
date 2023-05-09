06798bdbcd5ca55388026552c1edac52617e1059
Monte <monte6198@gmail.com>
2023-05-09 16:55:35 +0900

    feat: 회원가입 페이지 스타일 수정

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -50,112 +50,124 @@ const Register = () => {
 
   return (
     <>
-      <div className='register-contents'>
-        <div className='register-form'>
-          <h1>Register Youthting</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type='text'
-              className='input-text'
-              placeholder='이메일 입력'
-              required
-              autoFocus
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            <br />
-            <input
-              type='password'
-              className='input-text'
-              placeholder='비밀번호 입력'
-              required
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            <br />
-            <input
-              type='password'
-              className='input-text'
-              placeholder='비밀번호 확인'
-              required
-              value={password}
-              onChange={(event) => setConfirmPassword(event.target.value)}
-            />
-            <br />
-            <input
-              type='text'
-              className='input-text'
-              placeholder='이름 입력'
-              required
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-            <div className='radio-buttons'>
+      <div className='register-container'>
+        <h1 style={{ marginBottom: '1rem' }}>Register Youthting</h1>
+        <div className='register-contents'>
+          <div className='register-form' style={{ paddingLeft: '5rem' }}>
+            <form>
               <input
-                type='radio'
-                className='input-radio'
+                type='text'
+                className='input-text'
+                placeholder='이메일 입력'
                 required
-                value='Man'
-                checked={gender === 'Man'}
-                onChange={(event) => setGender(event.target.value)}
+                autoFocus
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
               />
-              <label className='radio-label'>남성</label>
               <br />
               <input
-                type='radio'
-                className='input-radio'
+                type='password'
+                className='input-text'
+                placeholder='비밀번호 입력'
                 required
-                value='Woman'
-                style={{ marginLeft: '2rem' }}
-                checked={gender === 'Woman'}
-                onChange={(event) => setGender(event.target.value)}
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
               />
-              <label className='radio-label'>여성</label>
-            </div>
-            <input
-              type='text'
-              className='input-text'
-              placeholder='나이 입력'
-              required
-              value={age}
-              onChange={(event) => setAge(event.target.value)}
-            />
-            <br />
-            <input
-              type='text'
-              className='input-text'
-              placeholder='키 입력'
-              required
-              value={height}
-              onChange={(event) => setHeight(event.target.value)}
-            />
-            <br />
-            <input
-              type='text'
-              className='input-text'
-              placeholder='MBTI 입력'
-              required
-              value={MBTI}
-              onChange={(event) => setMBTI(event.target.value)}
-            />
-            <br />
-            <input
-              type='text'
-              className='input-text'
-              placeholder='자기소개 입력'
-              required
-              value={introduction}
-              onChange={(event) => setIntroduction(event.target.value)}
-            />
-            <br />
-            <button className='register-button' disabled={loading}>
-              {loading ? '회원가입 중 ...' : '회원가입하기 !'}
-            </button>
-          </form>
+              <br />
+              <input
+                type='password'
+                className='input-text'
+                placeholder='비밀번호 확인'
+                required
+                value={password}
+                onChange={(event) => setConfirmPassword(event.target.value)}
+              />
+              <br />
+              <input
+                type='text'
+                className='input-text'
+                placeholder='이름 입력'
+                required
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+              <div className='radio-buttons'>
+                <input
+                  type='radio'
+                  className='input-radio'
+                  required
+                  value='Man'
+                  checked={gender === 'Man'}
+                  onChange={(event) => setGender(event.target.value)}
+                />
+                <label className='radio-label'>남성</label>
+                <br />
+                <input
+                  type='radio'
+                  className='input-radio'
+                  required
+                  value='Woman'
+                  style={{ marginLeft: '2rem' }}
+                  checked={gender === 'Woman'}
+                  onChange={(event) => setGender(event.target.value)}
+                />
+                <label className='radio-label'>여성</label>
+              </div>
+              <input
+                type='text'
+                className='input-text'
+                placeholder='나이 입력'
+                required
+                value={age}
+                onChange={(event) => setAge(event.target.value)}
+              />
+              <br />
+              <input
+                type='text'
+                className='input-text'
+                placeholder='키 입력'
+                required
+                value={height}
+                onChange={(event) => setHeight(event.target.value)}
+              />
+              <input
+                type='text'
+                className='input-text'
+                placeholder='MBTI 입력'
+                required
+                value={MBTI}
+                onChange={(event) => setMBTI(event.target.value)}
+              />
+            </form>
+          </div>
+          <div className='register-form' style={{ paddingRight: '5rem' }}>
+            <form>
+              <textarea
+                className='input-textarea'
+                cols='23'
+                rows='10'
+                placeholder='자기소개 입력'
+              ></textarea>
+              {/* <input
+                type='textarea'
+                className='input-text'
+                placeholder='자기소개 입력'
+                required
+                value={introduction}
+                onChange={(event) => setIntroduction(event.target.value)}
+              /> */}
+              <br />
+              <button className='register-button' disabled={loading}>
+                {loading ? '회원가입 중 ...' : '회원가입하기 !'}
+              </button>
+            </form>
+          </div>
         </div>
-        <Link className='to-login' to='/login'>
-          이미 계정이 있으신가요 ?
-        </Link>
+        <div className='to-login-div'>
+          <Link className='to-login' to='/login'>
+            이미 계정이 있으신가요 ?
+          </Link>
+        </div>
       </div>
     </>
   );
